0a0ef8017ea43a82162891ba353e7e0484978b52
jonatas <jonatasprado445@gmail.com>
2022-09-02 11:55:12 -0300

    cabeçario quase okay

---
+++ site/src/components/cabeçario/index.js
@@ -0,0 +1,44 @@
+
+import './index.scss';
+
+
+
+
+        export default function Cabecario(){
+        return(
+
+            <section className='page-cabecario'>
+                <div>
+                    
+                      </div>
+                <div className='sub1'>
+                    <img src="./assets/images/image 1067.png" />
+                    <h1> Overland</h1>
+
+                </div>
+                        
+                <div className='sub2'>
+
+                    <input type="text" placeholder="O que você procura? ;-;" />
+                    <img src='./assets/images/image 259.png' />
+                </div>
+
+                <div className='subx'>
+                    <img src='./assets/images/image 1066.png' alt='1' />
+                    
+
+                    <img src='./assets/images/image 1065.png' alt='1' />
+                    
+
+                    <img src='./assets/images/Star 14.png' alt='1' />
+                  
+                    
+                </div>
+              
+
+
+                
+            </section>
+        )
+        }
+
--- site/src/index.js
+++ site/src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import Home from './pages/home';
+import Cabecario from './components/cabeçario';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Cabecario/>
   </React.StrictMode>
 );
 
